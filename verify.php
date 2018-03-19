ca07516a0e0033f8e4f6e655b4ac0481413f775a
suwijak <suwijuk80@yahoo.com>
2018-03-20 00:58:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '70eifKrc4Mi8yGYsCXRcXluGy8e//eQfSt8ptK+kpqwJAdV+oUhrH+27lfVdjm9xC+pBugPFRo7Whv3r0m8TVpsjgwbPsb2izmfWv8+PK908Mz3iQWTOLkMiwuiLmP2G6P5eFFc0hnRd3oijAWoPHgdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v2/oauth/verify';
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Content-Type: application/x-www-form-urlencoded' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
