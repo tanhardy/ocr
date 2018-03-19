501e57977c76a0b059e7e600f6bed34462e5cad6
suwijak <suwijuk80@yahoo.com>
2018-03-20 00:36:53 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,12 @@
+<?php
+$access_token = '70eifKrc4Mi8yGYsCXRcXluGy8e//eQfSt8ptK+kpqwJAdV+oUhrH+27lfVdjm9xC+pBugPFRo7Whv3r0m8TVpsjgwbPsb2izmfWv8+PK908Mz3iQWTOLkMiwuiLmP2G6P5eFFc0hnRd3oijAWoPHgdB04t89/1O/w1cDnyilFU=';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
+?>
