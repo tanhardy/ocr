cb6746fbffe7777414f2d620fea57677d64bb8a5
suwijak <suwijuk80@yahoo.com>
2018-03-20 00:52:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -5,7 +5,6 @@ $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
